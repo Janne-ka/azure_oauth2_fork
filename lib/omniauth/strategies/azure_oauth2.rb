e568a78d88ce3ce34e1b8e4959189639454c3205
Janne-ka <44444166+Janne-ka@users.noreply.github.com>
2024-03-27 15:25:36 +0200

    edit to include both old and new endpoints as options
    with parameter old_endpoint=true solution goes to v1 endpoint

--- lib/omniauth/strategies/azure_oauth2.rb
+++ lib/omniauth/strategies/azure_oauth2.rb
@@ -5,7 +5,8 @@ module OmniAuth
   module Strategies
     class AzureOauth2 < OmniAuth::Strategies::OAuth2
       BASE_AZURE_URL = 'https://login.microsoftonline.com'
-
+      DEFAULT_SCOPE = 'openid profile email'
+      
       option :name, 'azure_oauth2'
 
       option :tenant_provider, nil
@@ -17,10 +18,10 @@ module OmniAuth
       args [:tenant_provider]
 
       def client
-        if options.tenant_provider
-          provider = options.tenant_provider.new(self)
+        provider = if options.tenant_provider
+          options.tenant_provider.new(self)
         else
-          provider = options  # if pass has to config, get mapped right on to options
+          options
         end
 
         options.client_id = provider.client_id
@@ -30,44 +31,97 @@ module OmniAuth
         options.base_azure_url =
           provider.respond_to?(:base_azure_url) ? provider.base_azure_url : BASE_AZURE_URL
 
-        options.authorize_params = provider.authorize_params if provider.respond_to?(:authorize_params)
-        options.authorize_params.domain_hint = provider.domain_hint if provider.respond_to?(:domain_hint) && provider.domain_hint
-        options.authorize_params.prompt = request.params['prompt'] if defined? request && request.params['prompt']
-        options.client_options.authorize_url = "#{options.base_azure_url}/#{options.tenant_id}/oauth2/authorize"
-        options.client_options.token_url = "#{options.base_azure_url}/#{options.tenant_id}/oauth2/token"
+        if provider.respond_to?(:authorize_params)
+          options.authorize_params = provider.authorize_params
+        end
+
+        if provider.respond_to?(:domain_hint) && provider.domain_hint
+          options.authorize_params.domain_hint = provider.domain_hint
+        end
+
+        if defined?(request) && request.params['prompt']
+          options.authorize_params.prompt = request.params['prompt']
+        end
+
+        options.authorize_params.scope = if provider.respond_to?(:scope) && provider.scope
+          provider.scope
+        else
+          DEFAULT_SCOPE
+        end
+
+        options.custom_policy =
+          provider.respond_to?(:custom_policy) ? provider.custom_policy : nil
+
+        if request.url.include?('old_endpoint=true')
+            options.client_options.authorize_url = "#{options.base_azure_url}/#{options.tenant_id}/oauth2/authorize"
+            options.client_options.token_url =
+              if options.custom_policy
+                "#{options.base_azure_url}/#{options.tenant_id}/#{options.custom_policy}/oauth2/token"
+              else
+                "#{options.base_azure_url}/#{options.tenant_id}/oauth2/token"
+              end
+        else
+
+          options.client_options.authorize_url = "#{options.base_azure_url}/#{options.tenant_id}/oauth2/v2.0/authorize"
+          options.client_options.token_url =
+            if options.custom_policy
+              "#{options.base_azure_url}/#{options.tenant_id}/#{options.custom_policy}/oauth2/v2.0/token"
+            else
+              "#{options.base_azure_url}/#{options.tenant_id}/oauth2/v2.0/token"
+            end
+        end
         super
       end
 
-      uid {
-        raw_info['sub']
-      }
+      uid { raw_info['oid'] }
 
       info do
         {
           name: raw_info['name'],
+          email: raw_info['email'] || raw_info['upn'],
           nickname: raw_info['unique_name'],
           first_name: raw_info['given_name'],
-          last_name: raw_info['family_name'],
-          email: raw_info['email'] || raw_info['upn'],
-          oid: raw_info['oid'],
-          tid: raw_info['tid']
+          last_name: raw_info['family_name']
         }
       end
 
-      def token_params
-        azure_resource = request.env['omniauth.params'] && request.env['omniauth.params']['azure_resource']
-        super.merge(resource: azure_resource || options.resource)
+      extra do
+        { raw_info: raw_info }
       end
 
       def callback_url
-        full_host + script_name + callback_path
+        full_host + callback_path
       end
 
+      # https://docs.microsoft.com/en-us/azure/active-directory/develop/id-tokens
+      #
+      # Some account types from Microsoft seem to only have a decodable ID token,
+      # with JWT unable to decode the access token. Information is limited in those
+      # cases. Other account types provide an expanded set of data inside the auth
+      # token, which does decode as a JWT.
+      #
+      # Merge the two, allowing the expanded auth token data to overwrite the ID
+      # token data if keys collide, and use this as raw info.
+      #
       def raw_info
-        # it's all here in JWT http://msdn.microsoft.com/en-us/library/azure/dn195587.aspx
-        @raw_info ||= ::JWT.decode(access_token.token, nil, false).first
-      end
+        if @raw_info.nil?
+          id_token_data = begin
+            ::JWT.decode(access_token.params['id_token'], nil, false).first
+          rescue StandardError
+            {}
+          end
+          auth_token_data = begin
+            ::JWT.decode(access_token.token, nil, false).first
+          rescue StandardError
+            {}
+          end
 
+          id_token_data.merge!(auth_token_data)
+          @raw_info = id_token_data
+        end
+
+        @raw_info
+      end
     end
   end
 end
